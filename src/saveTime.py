20f581bc9d57e1431247ef626aa06658572c8610
AlexSchwamle <alschwamle@gmail.com>
2025-04-22 23:04:58 -0600

    Save 12h time next to project identifier

--- src/saveTime.py
+++ src/saveTime.py
@@ -3,8 +3,9 @@ from os import makedirs
 
 def savePrintedTimes(printedTimes, nameOfLog):
     currentDayForFile = strftime("%Y-%m-%d")
+    currentTimeForProjectHeader = strftime("%I:%M %p")
     makedirs("../savedLogs", exist_ok=True)
     with open(f"../savedLogs/{currentDayForFile}.txt", "a") as file:
-        file.write(f"Project: {nameOfLog}\n")
+        file.write(f"Project: {nameOfLog} ({currentTimeForProjectHeader})\n")
         for printedLine in printedTimes:
             file.write("\t" + printedLine + "\n")
