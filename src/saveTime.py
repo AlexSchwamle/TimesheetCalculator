19092a069cf67f10773647bec9a563f075707c0c
AlexSchwamle <alschwamle@gmail.com>
2025-04-29 23:24:07 -0600

    Allow for utf-8 emoji chars in project names

--- src/saveTime.py
+++ src/saveTime.py
@@ -5,7 +5,7 @@ def savePrintedTimes(printedTimes, nameOfLog):
     currentDayForFile = strftime("%Y-%m-%d")
     currentTimeForProjectHeader = strftime("%I:%M %p")
     makedirs("../savedLogs", exist_ok=True)
-    with open(f"../savedLogs/{currentDayForFile}.txt", "a") as file:
+    with open(f"../savedLogs/{currentDayForFile}.txt", "a", encoding="utf-8") as file:
         file.write(f"Project: {nameOfLog} ({currentTimeForProjectHeader})\n")
         for printedLine in printedTimes:
             file.write("\t" + printedLine + "\n")
