c513bee2453666e54f1a501aad293bd2ba4872f3
AlexSchwamle <alschwamle@gmail.com>
2025-05-02 11:31:59 -0600

    Gracefully exit ctrl+c presses

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import printTime
 import tests.test
 from saveTime import savePrintedTimes
 
-if __name__ == "__main__":
+def run():
     nameOfLog = input("Enter the name of the project to save this time under: ") 
 
     print("Enter hours worked throughout each shift in the format 5:12 -> 9:32:\n")
@@ -22,3 +22,9 @@ if __name__ == "__main__":
     savePrintedTimes(printedTimes, nameOfLog)
 
     input("Press Enter or the upper right X to terminate script.\n")
+
+if __name__ == "__main__":
+    try:
+        run()
+    except KeyboardInterrupt:
+        print("\nCtrl+C pressed, exiting...")
